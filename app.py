1927dc62b7de2df3393a953cb804c2b6a6dd08a3
Patrick O Neill <patrickonei@gmail.com>
2019-02-04 16:37:10 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+#lib that will check the letter sequence and check close matches
+from difflib import SequenceMatcher, get_close_matches
+
+data = json.load(open('data.json'))
+
+def translate(w):
+    w=w.lower()
+    near = get_close_matches(w, data.keys())
+    
+    if w in data:
+        return data[w]
+    elif len(near) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % near[0])
+        if yn == "Y":
+            return data[near[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+    
+word = input("Enter word: ")
+
+output = (translate(word))
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
