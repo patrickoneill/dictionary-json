45022c9952ee1e9864f5f47dea2282a612d43bab
Patrick O Neill <patrickonei@gmail.com>
2019-02-04 16:53:47 +0000

    comments

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ def translate(w):
     
     if w in data:
         return data[w]
+        #returns the word if it is in the dictionary
     elif w.title() in data:
         return data[w.title()]
+        #returns the word if it starts with a capital
     elif w.upper() in data:
         return data[w.upper()]
+        #returns the word if it is in all capitals
     elif len(near) > 0:
+        #checks the length, how close a match and if it was what was looked for 
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % near[0])
         if yn == "Y":
             return data[near[0]]
@@ -27,6 +31,7 @@ def translate(w):
     
 word = input("Enter word: ")
 
+#prints the output in a line unlike json format
 output = (translate(word))
 if type(output) == list:
     for item in output:
